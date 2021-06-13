4595ab87c5df1bc7c3235ecde1cacf32ac85984b
Andrew Barron <hello@awmb.uk>
2021-06-13 11:13:24 +0100

    Switch to new URLs

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ async function handleRequest(request) {
 
 async function isUrlCachable(url) {
     const urlObj = new URL(url);
-    return     url.pathname.startsWith("/cms/shows")
-            || url.pathname.startsWith("/cms/date")
-            || url.pathname.startsWith("/cms/persons")
-            || url.pathname.startsWith("/cms/schedule")
-            || url.pathname.startsWith("/cms/sponsors")
+    return     url.pathname.startsWith("/shows")
+            || url.pathname.startsWith("/date")
+            || url.pathname.startsWith("/persons")
+            || url.pathname.startsWith("/schedule")
+            || url.pathname.startsWith("/sponsors")
 }
 
 addEventListener("fetch", event => {
