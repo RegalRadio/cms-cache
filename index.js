1483875659e29dbe4ef61ab8f6594898c2313591
Andrew Barron <hello@awmb.uk>
2023-12-14 20:29:48 +0000

    Generate all Cache-Control headers in this Worker, even if the item is straight from the origin

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ async function handleRequest(request) {
         // If request is cacheable, cache it for next time
         if (request.method === "GET" && isUrlCachable(request.url))
             await cache.put(request.url, response.clone());
-    } else if (response.headers.get("Cache-Max-Age")) {
-        // If serving a response from the cache, we need to update the Cache-Control headers, since Cloudflare
-        // will have overridden them with the domain-level default.  We can calculate the correct browser cache
-        // max age using the Cache-Max-Age provided by the Regal Radio CMS (which Cloudflare won't fiddle with
-        // since it's not a standard cache header) and the age of the cached item. If there's no Cache-Max-Age
-        // header, we skip this and assume the Cloudflare override is okay.
-
+    } 
+    
+    /*  
+    When pulling from an edge cache, the Cache-Control header is overridden by Cloudflare and set to the domain-wide default. So, instead of using Cache-Control, the CMS gives a max-age number in the Cache-Max-Age header. Since this is a non-standard header, Cloudflare leaves it alone. In the next statement, we change the Cache-Max-Age number into a proper Cache-Control header the browser can understand.
+    
+    We also subtract the age of the cached item from the original max-age before we give a number to the browser. This is because some CMS items are served with a very precise max-age (for example, the date endpoint give a max age time-til-next-midnight, since that's the next time the date will change). We need to adjust these values before we pass them to the browser cache so they expire from the browser cache at the same time they expire from the edge cache. For some items which just have a default standard cache time, this means browser cache expiration might be shorter than it could be, but it's not a major concern.
+    */
+    if (response.headers.get("Cache-Max-Age")) {
         response = new Response(response.body, response); // Rebuild response object so headers are mutable.
-        let newMaxAge = parseInt(response.headers.get("Cache-Max-Age")) - parseInt(response.headers.get("age"))
+        let newMaxAge = parseInt(response.headers.get("Cache-Max-Age")) - (parseInt(response.headers.get("Age") || 0)
         response.headers.set("Cache-Control", "public, must-revalidate, max-age=" + newMaxAge);
     }
 
